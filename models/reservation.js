bba72d45b572dd9417741117ef8f59f92b8ac1ba
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-28 19:26:55 -0700

    save method added to class Reservation, client will now be able to save/update a reservation

--- models/reservation.js
+++ models/reservation.js
@@ -14,13 +14,13 @@ class Reservation {
     this.numGuests = numGuests;
     this.startAt = startAt;
     this.notes = notes;
-  }
+  };
 
   /** formatter for startAt */
 
   getformattedStartAt() {
     return moment(this.startAt).format('MMMM Do YYYY, h:mm a');
-  }
+  };
 
   /** given a customer id, find their reservations. */
 
@@ -37,8 +37,29 @@ class Reservation {
     );
 
     return results.rows.map(row => new Reservation(row));
-  }
-}
+  };
 
+
+  /** save reservation */
+  async save() {
+    if(this.id === undefined) {
+      const result = await db.query(
+        `INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+        VALUES 
+        ($1, $2, $3, $4)
+        RETURNING id`,
+        [ this.customerId, this.startAt, this.numGuests, this.notes ]
+      );
+      this.id = result.rows[0].id;
+    } else {
+      db.query(
+        `UPDATE reservations
+        SET customer_id=$1 start_at=$2 num_guests=$3 notes=$4
+        WHERE id=$5`,
+        [ this.customerId, this.startAt, this.numGuests, this.notes, this.id ]
+      );
+    };
+  };
+};
 
 module.exports = Reservation;
